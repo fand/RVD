1c73831c5c92e26e226a264ad7202fa2b1c1419e
fand <fand@gmork.in>
2014-10-05 06:46:07 +0900

    play video

--- app/scripts/views/SampleList.js
+++ app/scripts/views/SampleList.js
@@ -3,14 +3,21 @@
 
 var React = require('react');
 var c = require('../Caret');
+var Mousetrap = require('mousetrap');
 
 var Sample = React.createClass({
+  componentDidMount: function () {
+    var self = this;
+    Mousetrap.bind('space', function () {
+      self.toggle();
+    });
+  },
+  toggle: function () {
+    var dom = this.refs.dom.getDOMNode();
+    (dom.paused) ?  dom.play() : dom.pause();
+  },
   render: function () {
-    return (
-      <video className="sample">
-        <source src={this.props.sample.url} />
-      </video>
-    );
+    return (<video src={this.props.sample.url} ref="dom" />);
   }
 });
 
@@ -22,5 +29,6 @@ var SampleList = React.createClass({
     return c.div({className: 'sampleList'}, this.props.samples.map(renderSample));
   }
 });
+
 
 module.exports = SampleList;
