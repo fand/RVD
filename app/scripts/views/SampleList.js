980cc9e9afc238d616c055ed9686be87779acd98
fand <fand@gmork.in>
2014-10-05 07:08:32 +0900

    toggle play/pause for multiple videos

--- app/scripts/views/SampleList.js
+++ app/scripts/views/SampleList.js
@@ -6,15 +6,11 @@ var c = require('../Caret');
 var Mousetrap = require('mousetrap');
 
 var Sample = React.createClass({
-  componentDidMount: function () {
-    var self = this;
-    Mousetrap.bind('space', function () {
-      self.toggle();
-    });
+  play: function () {
+    this.refs.dom.getDOMNode().play();
   },
-  toggle: function () {
-    var dom = this.refs.dom.getDOMNode();
-    (dom.paused) ?  dom.play() : dom.pause();
+  pause: function () {
+    this.refs.dom.getDOMNode().pause();
   },
   render: function () {
     return (<video src={this.props.sample.url} ref="dom" />);
@@ -22,9 +18,36 @@ var Sample = React.createClass({
 });
 
 var SampleList = React.createClass({
+  getInitialState: function () {
+    return {
+      paused: true
+    };
+  },
+  componentDidMount: function () {
+    var self = this;
+    Mousetrap.bind('space', function () {
+      this.toggle();
+    }.bind(this));
+  },
+  play: function () {
+    this.props.samples.forEach(function (sample, i) {
+      this.refs['sample' + i].play();
+    }.bind(this));
+  },
+  pause: function () {
+    this.props.samples.forEach(function (sample, i) {
+      this.refs['sample' + i].pause();
+    }.bind(this));
+  },
+  toggle: function () {
+    (this.state.paused) ? this.play() : this.pause();
+    this.setState({
+      paused: !this.state.paused
+    });
+  },
   render: function() {
-    var renderSample = function(sample) {
-      return (<Sample sample={sample} />);
+    var renderSample = function(sample, i) {
+      return (<Sample sample={sample} ref={'sample' + i} />);
     };
     return c.div({className: 'sampleList'}, this.props.samples.map(renderSample));
   }
