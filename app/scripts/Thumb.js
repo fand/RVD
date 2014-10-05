f8090ac7795352bdce653739c1db1a531ac97dae
fand <fand@gmork.in>
2014-10-05 16:24:03 +0900

    fix Thumb size

--- app/scripts/Thumb.js
+++ app/scripts/Thumb.js
@@ -6,8 +6,8 @@ canvas.style.opacity = 0;
 document.body.appendChild(canvas);
 
 var Thumb = function (video) {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
   return canvas.toDataURL();
 };
