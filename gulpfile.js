c4a97722cdff13638383e98f8b6706e6b436ddba
fand <fand@gmork.in>
2014-10-07 00:39:16 +0900

    fix indent

--- gulpfile.js
+++ gulpfile.js
@@ -7,75 +7,75 @@ var $ = require('gulp-load-plugins')();
 
 // Styles
 gulp.task('styles', function () {
-    return gulp.src('app/styles/main.scss')
-        .pipe($.rubySass({
-            style: 'expanded',
-            precision: 10,
-            loadPath: ['app/bower_components']
-        }))
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('dist/styles'))
-        .pipe($.size());
+  return gulp.src('app/styles/main.scss')
+    .pipe($.rubySass({
+      style: 'expanded',
+      precision: 10,
+      loadPath: ['app/bower_components']
+    }))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe($.size());
 });
 
 // CoffeeScript
 gulp.task('coffee', function () {
-    return gulp.src(
-            ['app/scripts/**/*.coffee', '!app/scripts/**/*.js'],
-            {base: 'app/scripts'}
-        )
-        .pipe(
-            $.coffee({ bare: true }).on('error', $.util.log)
-        )
-        .pipe(gulp.dest('app/scripts'));
+  return gulp.src(
+    ['app/scripts/**/*.coffee', '!app/scripts/**/*.js'],
+    {base: 'app/scripts'}
+  )
+    .pipe(
+      $.coffee({ bare: true }).on('error', $.util.log)
+    )
+    .pipe(gulp.dest('app/scripts'));
 });
 
 // Scripts
 gulp.task('scripts', function () {
-    return gulp.src('app/scripts/app.js')
-        .pipe($.browserify({
-            insertGlobals: true,
-            transform: ['reactify']
-        }))
-        .pipe($.plumber())
-        .pipe(gulp.dest('dist/scripts'))
-        .pipe($.size());
-    });
+  return gulp.src('app/scripts/app.js')
+    .pipe($.browserify({
+      insertGlobals: true,
+      transform: ['reactify']
+    }))
+    .pipe($.plumber())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe($.size());
+});
 
 // HTML
 gulp.task('html', function () {
-    return gulp.src('app/*.html')
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
+  return gulp.src('app/*.html')
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'))
+    .pipe($.size());
 });
 
 // Images
 gulp.task('images', function () {
-    return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size());
 });
 
 // Clean
 gulp.task('clean', function () {
-    return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
+  return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
 });
 
 
 // Bundle
 gulp.task('bundle', ['styles', 'scripts', 'bower'], function(){
-    var assets = $.useref.assets();
-    return gulp.src('./app/*.html')
-               .pipe(assets)
-               .pipe(assets.restore())
-               .pipe($.useref())
-               .pipe(gulp.dest('dist'));
+  var assets = $.useref.assets();
+  return gulp.src('./app/*.html')
+    .pipe(assets)
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'));
 });
 
 // Build
@@ -83,48 +83,48 @@ gulp.task('build', ['html', 'bundle', 'images']);
 
 // Default task
 gulp.task('default', ['clean'], function () {
-    gulp.start('build');
+  gulp.start('build');
 });
 
 // Webserver
 gulp.task('webserver', function () {
   gulp.src('dist')
-  .pipe($.webserver({
-    open: true,
-    port: 9000,
-    livereload: true
-  }));
+    .pipe($.webserver({
+      open: true,
+      port: 9000,
+      livereload: true
+    }));
 });
 
 // Bower helper
 gulp.task('bower', function() {
-    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
-        .pipe(gulp.dest('dist/bower_components/'));
+  gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+    .pipe(gulp.dest('dist/bower_components/'));
 
 });
 
 gulp.task('json', function() {
-    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
-        .pipe(gulp.dest('dist/scripts/'));
+  gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+    .pipe(gulp.dest('dist/scripts/'));
 });
 
 // Watch
 gulp.task('watch', ['html', 'bundle', 'webserver'], function () {
-    // Watch .json files
-    gulp.watch('app/scripts/**/*.json', ['json']);
+  // Watch .json files
+  gulp.watch('app/scripts/**/*.json', ['json']);
 
-    // Watch .html files
-    gulp.watch('app/*.html', ['html']);
+  // Watch .html files
+  gulp.watch('app/*.html', ['html']);
 
-    // Watch .scss files
-    gulp.watch('app/styles/**/*.scss', ['styles']);
+  // Watch .scss files
+  gulp.watch('app/styles/**/*.scss', ['styles']);
 
-    // Watch .coffeescript files
-    gulp.watch('app/scripts/**/*.coffee', ['coffee', 'scripts']);
+  // Watch .coffeescript files
+  gulp.watch('app/scripts/**/*.coffee', ['coffee', 'scripts']);
 
-    // Watch .js files
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
+  // Watch .js files
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
 
-    // Watch image files
-    gulp.watch('app/images/**/*', ['images']);
+  // Watch image files
+  gulp.watch('app/images/**/*', ['images']);
 });
