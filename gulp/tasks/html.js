3f7b4e59a94ddbca078f1c82cee0fe3494b5d71a
fand <fand@gmork.in>
2014-10-08 02:22:09 +0900

    use gulp-plumber

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -4,6 +4,7 @@ var $ = require('gulp-load-plugins')();
 // HTML
 gulp.task('html', function () {
   return gulp.src('app/*.html')
+    .pipe($.plumber())
     .pipe($.useref())
     .pipe(gulp.dest('dist'))
     .pipe($.size());
