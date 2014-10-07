5dc02aac1c2bf960ee71425b9869660a2d97f14e
fand <fand@gmork.in>
2014-10-07 19:54:23 +0900

    clean

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var React = require('react');
-var c = require('./Caret');
 var RVDApp = require('./views/RVDApp');
 
 React.renderComponent(<RVDApp />, document.getElementById('app'));
