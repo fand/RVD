407ba165a85ab55dff4829f90f4a40d84172174f
fand <s0811489@coins.tsukuba.ac.jp>
2014-10-18 05:43:43 +0900

    move Dropper / make Header clickable

---
+++ app/scripts/components/Header.js
@@ -0,0 +1,25 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = require('react');
+var Help = require('./Help');
+var HelpActions = require('../actions/HelpActions');
+
+var Header = React.createClass({
+  onClickHelp: function () {
+    HelpActions.toggleHelp();
+  },
+  render: function () {
+    return (
+      <div>
+        <div className="header">
+          <h1>RVD</h1>
+          <h2 onClick={this.onClickHelp}>HELP</h2>
+        </div>
+        <Help />
+      </div>
+    );
+  }
+});
+
+module.exports = Header;
---
+++ app/scripts/stores/HelpStore.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var React = require('react');
+var EventEmitter = require('events');
+var merge = require('react/lib/merge');
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var Constants = require('../Constants');
+var CHANGE_EVENT = 'CHANGE_HELPSTORE';
+
+var HelpStore = merge(EventEmitter.prototype, {
+  emitChange: function () {
+    this.emit(CHANGE_EVENT);
+  },
+  addListener: function (callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+  removeListener: function (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  // イベント受信時の動作を登録
+  dispatcherIndex: AppDispatcher.register(function (payload) {
+    var action = payload.action;
+
+    switch (action.actionType) {
+    case Constants.HELP_TOGGLE:
+      this.emitChange();
+      break;
+    }
+  })
+});
+
+
+module.exports = HelpStore;
