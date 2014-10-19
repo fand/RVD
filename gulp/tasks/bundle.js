4d25e67fef7f0e95dfeae0dd10ddab1e2f3ad076
fand <s0811489@coins.tsukuba.ac.jp>
2014-10-19 18:29:00 +0900

    remove bower

--- gulp/tasks/bundle.js
+++ gulp/tasks/bundle.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 // Bundle
-gulp.task('bundle', ['styles', 'scripts', 'bower'], function(){
+gulp.task('bundle', ['styles', 'scripts'], function(){
   var assets = $.useref.assets();
   return gulp.src('./app/*.html')
     .pipe($.plumber())
